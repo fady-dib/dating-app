1715dc543707938c4429a437b374b0bc7e8b0c89
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-20 05:45:48 +0200

    fix signup design

--- frontend/script.js
+++ frontend/script.js
@@ -27,3 +27,41 @@ if(!regex_email.test(email)){
         console.log(error)
     })
 }
+
+const register =() =>{
+    const first_name = document.getElementById('firstname').value;
+    const last_name = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const gender_id = document.querySelector('input[name="gender"]:checked').value;
+
+   const formatDate = () => {
+    const date = dob.toLocaleString('default', {year: 'numeric', month: '2-digit', day: '2-digit'})
+    return date
+   }
+
+   console.log(formatDate());
+    
+
+    let data = new FormData();
+    data.append('first_name', first_name);
+    data.append('last_name', last_name)
+    data.append('email', email);
+    data.append('password', password)
+    data.append('dob', formatDate())
+    data.append('gender_id', gender_id)
+
+    axios.post(`${baseUrl}register`, data).then(function(res){
+        if (res.data.message == "User with this email already exists"){
+            alert('Already registred')
+        }
+        if(res.data.message == "User created successfully"){
+            alert('Registered successfully')
+            window.localStorage.setItem('token',res.data.authorisation.token)
+            // location.replace('./index.html')
+        }
+    }).catch(function(error){
+        console.log(error)
+})
+}
