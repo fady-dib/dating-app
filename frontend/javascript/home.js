25a30e217033e56a354a8251f74561b37f918240
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-21 23:36:57 +0200

    add to favorites

--- frontend/javascript/home.js
+++ frontend/javascript/home.js
@@ -18,8 +18,45 @@ window.onload = () => {
             console.log(data)
             for(i=0;i<data.length;i++){
                 const name=data[i].first_name+" "+data[i].last_name;
+                const bio = data[i].bio;
+                const id = data[i].id;
+                html = ` <div class="user">
+                <img class="image" src="images/1.jpeg">
+                <h3 class="name">${name}</h3>
+                <div class="bio">${bio}<p>
+                    </p>
+                </div>
+                <div class="btn">
+                    <button class="favorite" onClick="favorite(${id})">Favorite</button>
+                    <button onClick="message(${id})"class="message">Message</button>
+                </div>
+            </div>`
+            container.insertAdjacentHTML('beforeend', html)
             }
         }
     })
+}
+
+const favorite=(id)=>{
+    const data = new FormData();
+    data.append('favorite_id', id)
+    axios.post(`${base_url}favorite`, data, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(function (res){
+        if(res.data.status == "failed"){
+            alert('this user is already in your favorites')
+        }
+        else if(res.data.status == "success"){
+            alert('Added to your favorites')
+        }
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
+
+const message = (id)=>{
+
 }
 
